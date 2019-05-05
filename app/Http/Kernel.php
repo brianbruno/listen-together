ed458ad57205cb0a2efda94eb47ec754dd3ab068
Brian <brian.bruno@outlook.com>
2019-05-05 15:11:38 -0300

    Correção HTTPS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,8 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+
+        \App\Http\Middleware\HttpsProtocol::class,
     ];
 
     /**
@@ -35,6 +37,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
+            \App\Http\Middleware\HttpsProtocol::class,
         ],
 
         'api' => [

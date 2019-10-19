eb24a19e38fb803aac370c14cebea9f482783041
Brian Bruno <brian.bruno@outlook.com>
2019-10-19 16:33:19 -0300

    Fila automática

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,37 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $path = base_path();
+
+        $schedule->call(function() use($path) {
+            if (file_exists($path . '/queue.pid')) {
+                $pid = file_get_contents($path . '/queue.pid');
+                $result = exec("ps -p $pid --no-heading | awk '{print $1}'");
+                $run = $result == '' ? true : false;
+            } else {
+                $run = true;
+            }
+            if($run) {
+                $command = '/usr/bin/php -c ' . $path .'/php.ini ' . $path . '/artisan queue:work --tries=1 > /dev/null & echo $!';
+                $number = exec($command);
+                file_put_contents($path . '/queue.pid', $number);
+            }
+        })->name('monitor_queue_listener')->everyMinute();
+
+        $schedule->call(function() use($path) {
+            if (file_exists($path . '/queue-system.pid')) {
+                $pid = file_get_contents($path . '/queue-system.pid');
+                $result = exec("ps -p $pid --no-heading | awk '{print $1}'");
+                $run = $result == '' ? true : false;
+            } else {
+                $run = true;
+            }
+            if($run) {
+                $command = '/usr/bin/php -c ' . $path .'/php.ini ' . $path . '/artisan queue:work --queue=system --tries=1 > /dev/null & echo $!';
+                $number = exec($command);
+                file_put_contents($path . '/queue-system.pid', $number);
+            }
+        })->name('monitor_queuesystem_listener')->everyMinute();
     }
 
     /**
